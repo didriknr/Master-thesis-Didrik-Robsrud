7fbce383de814183bed4825bc5983ec0191d55de
didriknr <112872404+didriknr@users.noreply.github.com>
2023-02-27 15:10:03 +0100

    Create bridge_launch.py

---
+++ src/radar/bridge_launch.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    # ROS1 nodes
+    roscore = Node(
+        package='roscore',
+        executable='roscore'
+    )
+    ld.add_action(roscore)
+
+    laser_publisher = Node(
+        package='laser_publisher',
+        executable='laser_publisher'
+    )
+    ld.add_action(laser_publisher)
+
+    # ROS2 nodes
+    listener = Node(
+        package='demo_nodes_cpp',
+        executable='listener',
+        name='my_listener',
+        remappings=[
+            ('/scan', 'scan')
+        ]
+    )
+    ld.add_action(listener)
+
+    # ROS1-ROS2 bridge node
+    ros1_bridge = Node(
+        package='ros1_bridge',
+        executable='dynamic_bridge'
+    )
+    ld.add_action(ros1_bridge)
+
+    return ld
