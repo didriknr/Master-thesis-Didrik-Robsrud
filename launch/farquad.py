917101b9a8e6f1e637aa8006120ffea832108582
Didrik <didriknr@uia.no>
2023-03-01 15:56:14 +0100

    sucsessfully established ros1->ros2 for scan

--- launch/farquad.py
+++ launch/farquad.py
@@ -6,14 +6,14 @@ def chmod():
         os.system(f"sudo chmod 666 /dev/ttyACM{i}")
 command0 = f"source {os.getcwd().rstrip('/src/Master-thesis-Didrik-Robsrud/launch')}/devel/setup.bash; roslaunch arnold.launch"
 command1 = f"source /opt/ros/melodic/setup.bash; source /opt/ros/eloquent/setup.bash; ros2 launch tamago.py"
-#command2 = f"source /opt/ros/eloquent/setup.bash; rqt"
-#command3 = f"source /opt/ros/eloquent/setup.bash; rviz2"
+command2 = f"source /opt/ros/melodic/setup.bash; source /opt/ros/eloquent/setup.bash; ros2 topic echo /merge/scan sensor_msgs/LaserScan"
+
 chmod()
 
 
 subprocess.run(["gnome-terminal", "--", "bash", "-c", command0])
 subprocess.run(["gnome-terminal", "--", "bash", "-c", command1])
-#subprocess.run(["gnome-terminal", "--", "bash", "-c", command2])
+subprocess.run(["gnome-terminal", "--", "bash", "-c", command2])
 
 
 
